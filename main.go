c2f4ff6ce13c6a41d17dc5e50cbc83fb2a8453eb
Gaiisa <hamidullinvladimir23@gmail.com>
2024-05-31 17:45:15 +0500

    3 urok

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
+// Обработчик главной страницы.
 func home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Привет из Snippetbox"))
+	w.Write([]byte("Привет из Snippetbox"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+// Обработчик для создания новой заметки.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
 func main() {
+	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
+	// маршрутизаторе servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-
-    
-    log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
